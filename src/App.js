fbac4174530cb87208edcbfdee271c121321cd26
Govind B Chandran <govindchandran235@gmail.com>
2021-06-25 20:39:24 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
+
+      <Sidebar/>
       
     </div>
   );
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Sidebar.css';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import SearchIcon from '@material-ui/icons/Search';
+
 
 
 function Sidebar() {
     return (
         <div className="sidebar">
+
+            <TwitterIcon/>
             
         </div>
     )
