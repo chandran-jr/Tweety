26c2f2fd750cfdcf590b5d49a101cffce87d6544
Govind B Chandran <govindchandran235@gmail.com>
2021-06-25 20:08:34 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBR1oJhcX9sumG6ytShtvIKzYXdMYFzg-8",
+    authDomain: "tweety-e5e48.firebaseapp.com",
+    projectId: "tweety-e5e48",
+    storageBucket: "tweety-e5e48.appspot.com",
+    messagingSenderId: "803754162193",
+    appId: "1:803754162193:web:0d5a4c962f9145fc5159b0"
+  };
+
+  
