9e27535f329f486775e9da9c36977c5a8da8caa1
Govind B Chandran <govindchandran235@gmail.com>
2021-06-27 21:36:05 +0530

    Add files via upload

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,11 @@ import TwitterIcon from '@material-ui/icons/Twitter';
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
 import SearchIcon from '@material-ui/icons/Search';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 
 
@@ -17,11 +22,11 @@ function Sidebar() {
             <SidebarOption Icon={HomeRoundedIcon} title="Home"/>
             <SidebarOption Icon={SearchIcon} title="Explore"/>
             <SidebarOption Icon={NotificationsNoneIcon} title="Notifications"/>
-            <SidebarOption Icon={SearchIcon} title="Explore"/>
-            <SidebarOption Icon={SearchIcon} title="Explore"/>
-            <SidebarOption Icon={SearchIcon} title="Explore"/>
-            <SidebarOption Icon={SearchIcon} title="Explore"/>
-            <SidebarOption Icon={SearchIcon} title="Explore"/>
+            <SidebarOption Icon={MailOutlineIcon} title="Messages"/>
+            <SidebarOption Icon={BookmarkBorderIcon} title="Explore"/>
+            <SidebarOption Icon={ListAltIcon} title="Explore"/>
+            <SidebarOption Icon={PermIdentityIcon} title="Explore"/>
+            <SidebarOption Icon={MoreHorizIcon} title="Explore"/>
 
             
         </div>
