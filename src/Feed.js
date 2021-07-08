6da3569f648a5e0e8b6ead4a9962dad68b415fd7
Govind B Chandran <govindchandran235@gmail.com>
2021-07-08 22:20:33 +0530

    Add files via upload

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Feed.css';
 import TweetBox from './TweetBox';
+import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
 
 function Feed() {
     return (
@@ -8,6 +9,7 @@ function Feed() {
 
             <div className="feed__header">
             <h1>Home</h1>
+            <StarBorderOutlinedIcon className="feed__sparkle"/>
             </div>
 
             <TweetBox />
