98d030bde3b21319df25f3df9b9289ab2024edcb
Govind B Chandran <govindchandran235@gmail.com>
2021-07-08 21:08:58 +0530

    Add files via upload

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -8,6 +8,7 @@ function TweetBox() {
             <form>
                 <div className="tweetBox__input">
                     <Avatar src="https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/avatars/govind_chandran.jpg"/>
+                    <input placeholder="What's happening?"/>
                 </div>
                 <Button>Tweet</Button>
             </form>
