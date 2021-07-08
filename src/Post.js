e9e8b0ffcb5e43b4346a085bb9003efdee7eb454
Govind B Chandran <govindchandran235@gmail.com>
2021-07-08 23:36:39 +0530

    Add files via upload

--- src/Post.js
+++ src/Post.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Post.css';
+import {Avatar} from '@material-ui/core';
 
-function Post({displayName,userName,text,avatar}) {
+function Post({displayName,userName,verified,text,avatar}) {
     return (
         <div className="post">
-            
+            <div className="post__avatar">
+                <Avatar src="https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/avatars/govind_chandran.jpg"/>
+            </div>
         </div>
     )
 }
