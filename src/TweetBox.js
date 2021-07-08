8ff6ad2f8e4317e52a7a5fdbbd76b71410e285a6
Govind B Chandran <govindchandran235@gmail.com>
2021-07-08 21:08:07 +0530

    Add files via upload

---
+++ src/TweetBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './TweetBox.css';
+import {Button, Avatar} from '@material-ui/core';
+
+function TweetBox() {
+    return (
+        <div className="tweetBox">
+            <form>
+                <div className="tweetBox__input">
+                    <Avatar src="https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/avatars/govind_chandran.jpg"/>
+                </div>
+                <Button>Tweet</Button>
+            </form>
+        </div>
+    )
+}
+
+export default TweetBox
