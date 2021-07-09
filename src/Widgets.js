e3270eeb021640ed3feedfb9046673d46f8d4436
Govind B Chandran <govindchandran235@gmail.com>
2021-07-09 23:22:23 +0530

    Add files via upload

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Widgets.css';
-import {TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed} from 'react-twitter-embed';
 import SearchIcon from '@material-ui/icons/Search';
 
 function Widgets() {
@@ -8,7 +7,14 @@ function Widgets() {
         <div className="widgets">
         <div className="widgets__input">
             <SearchIcon className="widgets__searchIcon"/>
+            <input placeholder="Search Twitter" type="text"/>
         </div>
+
+        <div className="widgets__widgetContainer">
+            <h2>What's Happening?</h2>
+
+        </div>
+
         </div>
     )
 }
