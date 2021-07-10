ec67bb9e175eeee64e51f0e2e375aaa1683bec57
Govind B Chandran <govindchandran235@gmail.com>
2021-07-10 14:52:28 +0530

    Add files via upload

--- src/Widgets.js
+++ src/Widgets.js
@@ -5,9 +5,10 @@ import SearchIcon from '@material-ui/icons/Search';
 function Widgets() {
     return (
         <div className="widgets">
+
         <div className="widgets__input">
             <SearchIcon className="widgets__searchIcon"/>
-            <input placeholder="Search Twitter" type="text"/>
+            <input className="widgets__inputBox" placeholder="Search Twitter" type="text"/>
         </div>
 
         <div className="widgets__widgetContainer">
